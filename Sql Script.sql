931168de453a04e9cb8a5b33bc68885006007031
Perryblvk <Perrybabajide17@gmail.com>
2025-02-19 22:55:42 +0100

    Update Sql Script.sql

--- Sql Script.sql
+++ Sql Script.sql
@@ -43,3 +43,58 @@ select * from person.Person
 where BusinessEntityID in (1,2,3,4,5,6,7,8)
 Order by EmailPromotion desc
 ;
+SELECT * FROM movies_tb
+where Score > 70
+order by Score desc
+;
+-- Movies with action Genre and rated PG-13
+
+SELECT * FROM movies_tb
+where Genre = 'Action' and Rating = 'PG-13'
+;
+-- Movies released after 2010
+
+SELECT * FROM movies_tb
+where Release_Date > '2010-12-31'
+;
+-- OR
+
+SELECT * FROM movies_tb
+where year(Release_Date) > 2010
+order by Release_Date desc
+;
+-- No of movies by genre
+
+SELECT Genre, count(genre) as sum_of_genre
+from movies_tb
+group by Genre
+;
+-- Number of movies by director
+
+SELECT Director, count(Director) as no_of_movies_by_director
+from movies_tb
+group by Director
+order by no_of_movies_by_director desc
+;
+
+-- Average Score of movies by genre
+SELECT Genre, AVG(Score) as Average_Score
+from movies_tb
+group by Genre
+order by Average_Score desc
+;
+
+-- Full meaning of rating abbrevation
+Select Genre, Rating, Score,
+-- PG = Parental Guidance
+-- PG-13 = Parental Guidance 13
+-- G = General Audience
+-- R = Restricted
+CASE
+WHEN rating = 'PG' THEN 'Parental Guidance'
+when rating = 'PG-13' THEN 'Parental Guidance 13'
+when rating = 'G' THEN 'General Audience'
+else 'Restricted'
+END AS Rating_meaning
+from movies_tb
+;
